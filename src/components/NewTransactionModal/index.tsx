b0b9d7978409b38c3ffdfb0122e655ecdc03f4be
Alexandre Menezes <amenezes031@gmail.com>
2021-03-20 19:58:00 -0300

    :construction: structure form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -12,8 +12,31 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal 
       isOpen={isOpen} 
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input 
+          type="text"
+          placeholder="Título"
+        />
+
+        <input 
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input 
+          type="text"
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   );
 }
